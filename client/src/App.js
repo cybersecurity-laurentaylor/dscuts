7bc5c70a7a471ae4d1dc18eb0f89abc562e9b424
Lauren Taylor <laurentaylor3296@gmail.com>
2026-04-22 10:41:30 -0400

    initial files uploaded

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import { Provider } from "@/components/ui/provider"
+import './App.css';
+
+function App() {
+  return (
+    <Provider>
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    </Provider>
+  );
+}
+
+export default App;
